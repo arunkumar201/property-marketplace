79f27765f7e5eca9390d8c334009774ef4be3704
Arun Kumar <arunprajapati9825@gmail.com>
2024-10-06 16:50:24 +0530

    Fix current page updatation

--- src/providers/FilterProvider.tsx
+++ src/providers/FilterProvider.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { ROOM_OPTIONS } from "@/constants";
 import { useRouter,useSearchParams } from "next/navigation";
@@ -7,7 +7,7 @@ import React,{ createContext,useEffect,useState } from "react";
 export interface FilterContextType {
 	location: string;
 	type: string;
-	saleType: string; // propertyOn means - BUY or SELL 
+	saleType: string; // propertyOn means - BUY or SELL
 	priceMin: string;
 	priceMax: string;
 	areaMin: string;
@@ -17,7 +17,9 @@ export interface FilterContextType {
 	setFilter: (key: string,value: string | string[]) => void;
 }
 
-export const FilterContext = createContext<FilterContextType | undefined>(undefined);
+export const FilterContext = createContext<FilterContextType | undefined>(
+	undefined
+);
 
 export const FilterProvider: React.FC<{ children: React.ReactNode }> = ({
 	children,
@@ -25,17 +27,16 @@ export const FilterProvider: React.FC<{ children: React.ReactNode }> = ({
 	const searchParams = useSearchParams();
 	const router = useRouter();
 
-
 	const [filters,setFilters] = useState({
-		location: searchParams.get('location') ?? "",
-		type: searchParams.get('type') ?? "Haus",
-		saleType: searchParams.get('saleType') ?? "BUY",
-		priceMin: searchParams.get('priceMin') ?? "10",
-		priceMax: searchParams.get('priceMax') ?? "4000000",
-		areaMin: searchParams.get('areaMin') ?? "10",
-		areaMax: searchParams.get('areaMax') ?? "2000000",
-		rooms: searchParams.get('rooms')?.split(',') ?? ROOM_OPTIONS,
-		currentPage: searchParams.get('currentPage') ?? "1",
+		location: searchParams.get("location") ?? "",
+		type: searchParams.get("type") ?? "Haus",
+		saleType: searchParams.get("saleType") ?? "BUY",
+		priceMin: searchParams.get("priceMin") ?? "10",
+		priceMax: searchParams.get("priceMax") ?? "4000000",
+		areaMin: searchParams.get("areaMin") ?? "10",
+		areaMax: searchParams.get("areaMax") ?? "2000000",
+		rooms: searchParams.get("rooms")?.split(",") ?? ROOM_OPTIONS,
+		currentPage: searchParams.get("currentPage") ?? "1",
 	});
 
 	const setFilter = (key: string,value: string | string[]) => {
@@ -45,38 +46,54 @@ export const FilterProvider: React.FC<{ children: React.ReactNode }> = ({
 		}));
 	};
 
-	const applyFilters = () => {
+	const applyFilters = (setCurrentPage = false) => {
 		const params = new URLSearchParams();
 
-		if (filters.location || filters.location === "") params.set('location',filters.location);
-		if (filters.type) params.set('type',filters.type);
-		if (filters.saleType) params.set('saleType',filters.saleType);
-		if (filters.priceMin) params.set('priceMin',filters.priceMin);
-		if (filters.priceMax) params.set('priceMax',filters.priceMax);
-		if (filters.areaMin) params.set('areaMin',filters.areaMin);
-		if (filters.areaMax) params.set('areaMax',filters.areaMax);
-		if (filters.rooms) params.set('rooms',filters.rooms.join(','));
-		if (filters.currentPage) params.set('currentPage',filters.currentPage);
+		if (filters.location || filters.location === "")
+			params.set("location",filters.location);
+		if (filters.type) params.set("type",filters.type);
+		if (filters.saleType) params.set("saleType",filters.saleType);
+		if (filters.priceMin) params.set("priceMin",filters.priceMin);
+		if (filters.priceMax) params.set("priceMax",filters.priceMax);
+		if (filters.areaMin) params.set("areaMin",filters.areaMin);
+		if (filters.areaMax) params.set("areaMax",filters.areaMax);
+		if (filters.rooms) params.set("rooms",filters.rooms.join(","));
+		if (filters.currentPage) params.set("currentPage",setCurrentPage ? "1" : filters.currentPage);
 
 		router.push(`?${params.toString()}`);
-	}
+	};
+
+	useEffect(() => {
+		const setCurrentPage = true;
+		applyFilters(setCurrentPage);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	},[
+		filters.areaMax,
+		filters.areaMin,
+		filters.location,
+		filters.priceMax,
+		filters.rooms,
+		filters.priceMin,
+		filters.type,
+		filters.saleType,
+	]);
 
 	useEffect(() => {
 		applyFilters();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	},[filters]);
+	},[filters.currentPage]);
 
 	useEffect(() => {
 		const newFilters = {
-			location: searchParams.get('location') ?? "",
-			type: searchParams.get('type') ?? "Haus",
-			saleType: searchParams.get('saleType') ?? "SELL",
-			priceMin: searchParams.get('priceMin') ?? "100",
-			priceMax: searchParams.get('priceMax') ?? "40000000",
-			areaMin: searchParams.get('areaMin') ?? "10",
-			areaMax: searchParams.get('areaMax') ?? "20000000",
-			rooms: searchParams.get('rooms')?.split(',') ?? ROOM_OPTIONS,
-			currentPage: searchParams.get('currentPage') ?? "1",
+			location: searchParams.get("location") ?? "",
+			type: searchParams.get("type") ?? "Haus",
+			saleType: searchParams.get("saleType") ?? "SELL",
+			priceMin: searchParams.get("priceMin") ?? "100",
+			priceMax: searchParams.get("priceMax") ?? "40000000",
+			areaMin: searchParams.get("areaMin") ?? "10",
+			areaMax: searchParams.get("areaMax") ?? "20000000",
+			rooms: searchParams.get("rooms")?.split(",") ?? ROOM_OPTIONS,
+			currentPage: searchParams.get("currentPage") ?? "1",
 		};
 
 		setFilters(newFilters);
